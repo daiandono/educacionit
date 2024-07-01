f25a2847379523460752a29cc2cc6354e1757743
Daiana J. Vogel <djvogel@enargas.gov.ar>
2024-07-01 11:25:42 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var A = 10;
+var B = 5;
+if (A > B) {
+    console.log("".concat(A, " es mayor que ").concat(B));
+}
+else {
+    console.log("".concat(A, " es menor que ").concat(B));
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+const A: number = 10;
+const B: number = 5;
+
+if(A > B){
+    console.log(`${A} es mayor que ${B}`)
+}else{
+    console.log(`${A} es menor que ${B}`)
+}
